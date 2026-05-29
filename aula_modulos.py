c89d1724da5d67836862ed6cd313a92812eec515
Augusto abdo <selmoselmoabdo@yahoo.com>
2026-05-29 15:47:35 -0300

    Create aula_modulos.py

---
+++ aula_modulos.py
@@ -0,0 +1,4 @@
+import random
+#num  = random.random()#O random gera números aleatórios de 0 á 1:
+num = random.randint(10 , 100)# O randint importa números inteiros definidos no paranteses 
+print(num)
